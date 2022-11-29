f88984f32e34d95514ac1760a36bca8b28942411
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-29 16:02:39 -0300

    Eliminacion de Scripts que se combinarion en uno

--- Scripts/CreacionBD/2 Crear Tabla Idioma.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE domus.Idioma (
-	Nombre_Idioma varchar(20) NOT NULL,
-	CONSTRAINT Idioma_pk PRIMARY KEY (Nombre_Idioma)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci
-COMMENT='Idiomas del Mundo';
