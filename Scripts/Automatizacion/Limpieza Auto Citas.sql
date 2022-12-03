9a21ff01dfcf25acc4a4414ef4a3cf959e1a1bca
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-12-03 02:08:18 -0300

    Update Limpieza Auto Citas.sql
    Se mejoro el evento de Limpieza de citas

--- Scripts/Automatizacion/Limpieza Auto Citas.sql
+++ Scripts/Automatizacion/Limpieza Auto Citas.sql
@@ -1,6 +1,7 @@
-CREATE EVENT limpieza_citas 
-ON SCHEDULE EVERY 30 DAY STARTS '2023-01-02 06:00:00' 
-DO 
-delete 
+CREATE EVENT limpieza_citas
+ON SCHEDULE EVERY 1 DAY
+ON COMPLETION NOT PRESERVE
+ENABLE
+DO delete 
 from cita 
-where F_Concluye_Cita is not null and H_Concluye_Cita is not null;
+where (F_Concluye_Cita is not null and H_Concluye_Cita is not null) and (TIMESTAMPDIFF(day, timestamp(F_Concluye_Cita, H_Concluye_Cita), current_timestamp()) > 30); 
